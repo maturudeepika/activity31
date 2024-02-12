d9494cba4cd27052ce74a94478640a8dabd74db1
maturudeepika <136905658+maturudeepika@users.noreply.github.com>
2024-02-12 14:15:31 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask,request
+from model import predict_model
+app=Flask(__name__)
+
+
+@app.route('/')
+def basic():
+    return 'api server started'
+
+
+@app.route('/soil',methods=['get']) 
+def soil():  #sensor data calculating
+    value=request.args.get('soil')
+    print(value)
+    value=int(value)
+    response=predict_model(value)
+    return {'msg':response}
+
+if __name__=="__main__":
+    app.run('0.0.0.0',port=5001) #runs on all networks
